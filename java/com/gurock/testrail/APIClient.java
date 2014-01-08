a93074ae45bf26f647bd3a64db097b820028855c
Tobias Gurock <tg@gurock.com>
2014-01-08 13:06:10 +0100

    Working on HTTP request, JSON support still missing

--- java/com/gurock/testrail/APIClient.java
+++ java/com/gurock/testrail/APIClient.java
@@ -1,4 +1,24 @@
+/**
+ * TestRail API binding for Java (API v2, available since TestRail 3.0)
+ *
+ * Learn more:
+ *
+ * http://docs.gurock.com/testrail-api2/start
+ * http://docs.gurock.com/testrail-api2/accessing
+ *
+ * Copyright Gurock Software GmbH
+ */
+ 
 package com.gurock.testrail;
+
+import java.net.URL;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.BufferedReader;
 
 public class APIClient
 {
@@ -8,8 +28,19 @@ public class APIClient
 
 	public APIClient(String base_url)
 	{
+		if (base_url.endsWith("/"))
+		{
+			base_url += "/";
+		}
+		
+		this.m_url = base_url + "index.php?/api/v2/";
 	}
 
+	/**
+	 * Get/Set User
+	 *
+	 * Returns/sets the user used for authenticating the API requests.
+	 */
 	public String getUser()
 	{
 		return this.m_user;
@@ -20,6 +51,11 @@ public class APIClient
 		this.m_user = user;
 	}
 
+	/**
+	 * Get/Set Password
+	 *
+	 * Returns/sets the password used for authenticating the API requests.
+	 */
 	public String getPassword()
 	{
 		return this.m_password;
@@ -30,12 +66,136 @@ public class APIClient
 		this.m_password = password;
 	}
 
-	public void sendGet(String uri)
+	/**
+	 * Send Get
+	 *
+	 * Issues a GET request (read) against the API and returns the result
+	 * (as PHP array).
+	 *
+	 * Arguments:
+	 *
+	 * uri                  The API method to call including parameters
+	 *                      (e.g. get_case/1)
+	 */
+	public String sendGet(String uri)
+		throws MalformedURLException, IOException, APIException
 	{
+		return this.sendRequest("GET", uri, null);
 	}
 
-	public void sendPost(String url, Object data)
+	/**
+	 * Send POST
+	 *
+	 * Issues a POST request (write) against the API and returns the result
+	 * (as ...).
+	 *
+	 * Arguments:
+	 *
+	 * uri                  The API method to call including parameters
+	 *                      (e.g. add_case/1)
+	 * data                 The data to submit as part of the request (as
+	 *                      ..)
+	 */
+	public String sendPost(String uri, Object data)
+		throws MalformedURLException, IOException, APIException
 	{
+		return this.sendRequest("POST", uri, data);
+	}
+	
+	private String sendRequest(String method, String uri, Object data)
+		throws MalformedURLException, IOException, APIException
+	{
+		URL url = new URL(this.m_url + uri);
+		
+		// Create the connection object and set the required HTTP method
+		// (GET/POST) and headers (content type and basic auth).
+		HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+		conn.addRequestProperty("Content-Type", "application/json");
+		
+		String auth = getAuthorization(this.m_user, this.m_password);
+		conn.addRequestProperty("Authorization", "Basic " + auth);
+		
+		if (method == "POST")
+		{
+			// Add the POST arguments, if any. We just serialize the passed
+			// data object (i.e. a dictionary) and then add it to the
+			// request body.
+			if (data != null)
+			{
+				byte[] block = ((String) data).getBytes("UTF-8");
+
+				conn.setDoOutput(true);				
+				OutputStream ostream = conn.getOutputStream();			
+				ostream.write(block);
+				ostream.flush();
+			}
+		}
+		
+		// Execute the actual web request (GET or POST) and record any
+		// occurred errors (we use the error stream in this case).
+		int status = conn.getResponseCode();
+		
+		InputStream istream;
+		if (status != 200)
+		{
+			istream = conn.getErrorStream();
+			if (istream == null)
+			{
+				throw new APIException(
+					"TestRail API return HTTP " + status + 
+					" (No additional error message received)"
+				);
+			}
+		}
+		else 
+		{
+			istream = conn.getInputStream();
+		}
+		
+		// Read the response body, if any, and deserialize it from JSON.
+		String text = "";
+		if (istream != null)
+		{
+			BufferedReader reader = new BufferedReader(
+				new InputStreamReader(
+					istream,
+					"UTF-8"
+				)
+			);
+		
+			String line;
+			while ((line = reader.readLine()) != null)
+			{
+				text += line;
+				text += System.getProperty("line.separator");
+			}
+			
+			reader.close();
+		}
+		
+		if (text != "")
+		{
+			// TODO
+		}
+		else 
+		{
+		
+		}
+		
+		// Check for any occurred errors and add additional details to
+		// the exception message, if any (e.g. the error message returned
+		// by TestRail).
+		if (status != 200)
+		{
+			// TODO
+		}
+		
+		return text;
+	}
+	
+	private static String getAuthorization(String user, String password)
+	{
+		return "dGdAZ3Vyb2NrLmNvbTp0ZXN0dGVzdA==";
+		// return "dGdAZ3Vyb2NrLmNvbTpmb29iYXIK";
 	}
 }
-
