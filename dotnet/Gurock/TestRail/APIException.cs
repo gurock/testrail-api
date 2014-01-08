bfa87359c6c964d49784eecddf2fa6498c637942
Tobias Gurock <tg@gurock.com>
2014-01-08 02:14:22 -0800

    Added info header

--- dotnet/Gurock/TestRail/APIException.cs
+++ dotnet/Gurock/TestRail/APIException.cs
@@ -1,3 +1,14 @@
+/**
+ * TestRail API binding for .NET (API v2, available since TestRail 3.0)
+ *
+ * Learn more:
+ *
+ * http://docs.gurock.com/testrail-api2/start
+ * http://docs.gurock.com/testrail-api2/accessing
+ *
+ * Copyright Gurock Software GmbH
+ */
+
 using System;
 using System.Runtime.Serialization;
 
