4995dc60e6bc8ff8bfe10b53ad0c2f4e5f2fc153
Tobias Gurock <tg@gurock.com>
2014-01-08 16:08:25 +0100

    First complete version of Java binding

--- java/com/gurock/testrail/APIClient.java
+++ java/com/gurock/testrail/APIClient.java
@@ -19,6 +19,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.BufferedReader;
+import java.io.UnsupportedEncodingException;
 import org.json.simple.JSONObject;
 import org.json.simple.JSONValue;
 
@@ -100,9 +101,9 @@ public class APIClient
 	 *
 	 * uri                  The API method to call including parameters
 	 *                      (e.g. add_case/1)
-	 * data                 The data to submit as part of the request (as
-	 *                      serializable object)
-     *
+	 * data                 The data to submit as part of the request (e.g.,
+	 *                      a map)
+	 *
 	 * Returns the parsed JSON response as standard object which can
 	 * either be an instance of JSONObject or JSONArray (depending on the
 	 * API method). In most cases, this returns a JSONObject instance which
@@ -208,7 +209,7 @@ public class APIClient
 				JSONObject obj = (JSONObject) result;
 				if (obj.containsKey("error"))
 				{
-					error = (String) obj.get("error");
+					error = '"' + (String) obj.get("error") + '"';
 				}
 			}
 			
@@ -223,7 +224,80 @@ public class APIClient
 	
 	private static String getAuthorization(String user, String password)
 	{
-		return "dGdAZ3Vyb2NrLmNvbTp0ZXN0dGVzdA==";
-		// return "dGdAZ3Vyb2NrLmNvbTpmb29iYXIK";
+		try 
+		{
+			return getBase64((user + ":" + password).getBytes("UTF-8"));
+		}
+		catch (UnsupportedEncodingException e)
+		{
+			// Not thrown
+		}
+		
+		return "";
+	}
+	
+	private static String getBase64(byte[] buffer)
+	{
+		final char[] map = {
+			'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+			'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
+			'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd',
+			'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+			'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
+			'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7',
+			'8', '9', '+', '/'
+		};
+	
+		StringBuffer sb = new StringBuffer();
+		for (int i = 0; i < buffer.length; i++)
+		{
+			byte b0 = buffer[i++], b1 = 0, b2 = 0;
+
+			int bytes = 3;
+			if (i < buffer.length)
+			{
+				b1 = buffer[i++];
+				if (i < buffer.length)
+				{
+					b2 = buffer[i];
+				}
+				else 
+				{
+					bytes = 2;
+				}
+			}
+			else
+			{
+				bytes = 1;
+			}
+			
+			int total = (b0 << 16) | (b1 << 8) | b2;
+			
+			switch (bytes)
+			{
+				case 3:
+					sb.append(map[(total >> 18) & 0x3f]);
+					sb.append(map[(total >> 12) & 0x3f]);
+					sb.append(map[(total >> 6) & 0x3f]);
+					sb.append(map[total & 0x3f]);
+					break;
+					
+				case 2:
+					sb.append(map[(total >> 18) & 0x3f]);
+					sb.append(map[(total >> 12) & 0x3f]);
+					sb.append(map[(total >> 6) & 0x3f]);
+					sb.append('=');
+					break;
+					
+				case 1:
+					sb.append(map[(total >> 18) & 0x3f]);
+					sb.append(map[(total >> 12) & 0x3f]);
+					sb.append('=');
+					sb.append('=');
+					break;
+			}
+		}
+	
+		return sb.toString();
 	}
 }
--- java/com/gurock/testrail/APIException.java
+++ java/com/gurock/testrail/APIException.java
@@ -18,4 +18,3 @@ public class APIException extends Exception
 		super(message);
 	}
 }
-
