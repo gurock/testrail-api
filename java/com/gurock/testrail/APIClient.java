5ed8bfa8647c3d8515d7b166f238030e6f7e59c9
Tobias Gurock <tg@gurock.com>
2014-07-21 16:52:02 +0200

    Added license (MIT)

--- java/com/gurock/testrail/APIClient.java
+++ java/com/gurock/testrail/APIClient.java
@@ -6,7 +6,7 @@
  * http://docs.gurock.com/testrail-api2/start
  * http://docs.gurock.com/testrail-api2/accessing
  *
- * Copyright Gurock Software GmbH
+ * Copyright Gurock Software GmbH. See license.md for details.
  */
  
 package com.gurock.testrail;
