c5902bb75fff04a08fd7add65801b9c9625fb91e
Dai Fujihara <daipresents@gmail.com>
2019-06-04 22:01:10 +0900

    refactoring.

--- spec/lib/testrail/api/plans_spec.rb
+++ spec/lib/testrail/api/plans_spec.rb
@@ -3,28 +3,32 @@
 RSpec.describe 'TestRail' do
   context 'API' do
     context 'Plans' do
-      it 'can get/create/delete test plan' do
-        client = TestRail::Client.new(ENV['TESTRAIL_URL'])
+      before(:each) do
+        @client = TestRail::Client.new(ENV['TESTRAIL_URL'])
+        @name = 'Test Plan Name'
+        @description = 'Test Plan Description'
+      end
 
-        plan = client.add_plan('Clientでつくったやーつ', '説明もかけます')
+      it 'can get/create/delete test plan' do
+        plan = @client.add_plan(@name, @description)
         expect(plan).not_to be_nil
 
         plan_id = plan['id']
-        plan = client.get_plan(plan_id)
+        plan = @client.get_plan(plan_id)
         expect(plan['id']).to eq(plan_id)
 
-        client.delete_plan(plan_id)
-        expect { client.get_plan(plan_id) }.to raise_error(TestRail::APIError, 'TestRail API returned HTTP 400 ("Field :plan_id is not a valid test plan.")')
+        @client.delete_plan(plan_id)
+        expect { @client.get_plan(plan_id) }.to raise_error(TestRail::APIError, 'TestRail API returned HTTP 400 ("Field :plan_id is not a valid test plan.")')
       end
 
       it 'can close test plan' do
-        client = TestRail::Client.new(ENV['TESTRAIL_URL'])
-
-        plan = client.add_plan('Clientでつくったやーつ', '説明もかけます')
+        plan = @client.add_plan('Clientでつくったやーつ', '説明もかけます')
         plan_id = plan['id']
-        client.close_plan(plan_id)
+        expect(plan['is_completed']).to be false
 
-        plan = client.get_plan(plan_id)
+        @client.close_plan(plan_id)
+
+        plan = @client.get_plan(plan_id)
         expect(plan['is_completed']).to be true
       end
     end
