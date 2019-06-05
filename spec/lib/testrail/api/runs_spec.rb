e8ce73853944ad735206314549f1719e73796ab9
Dai Fujihara <daipresents@gmail.com>
2019-06-05 14:55:24 +0900

    add expectation to set up parameter case.

--- spec/lib/testrail/api/runs_spec.rb
+++ spec/lib/testrail/api/runs_spec.rb
@@ -12,13 +12,32 @@ RSpec.describe 'TestRail' do
       it 'get default payload' do
         payload = @client.get_run_payload
 
-        expect(payload[:suite_id]).to be_nil
         expect(payload[:name]).to be_nil
         expect(payload[:description]).to be_nil
+        expect(payload[:suite_id]).to be_nil
         expect(payload[:milestone_id]).to be_nil
         expect(payload[:assignedto_id]).to be_nil
         expect(payload[:include_all]).to be_nil
         expect(payload[:case_ids]).to be_nil
+        expect(payload[:unknown_parameter]).to be_nil
+
+        suite_id = 1
+        name = 'name'
+        description = 'description'
+        milestone_id = 1
+        assignedto_id = 1
+        include_all = true
+        case_ids = [1, 2, 3]
+
+        payload = @client.get_run_payload(name, description, suite_id, milestone_id, assignedto_id, include_all, case_ids)
+
+        expect(payload[:name]).to eq(name)
+        expect(payload[:description]).to eq(description)
+        expect(payload[:suite_id]).to eq(suite_id)
+        expect(payload[:milestone_id]).to eq(milestone_id)
+        expect(payload[:assignedto_id]).to eq(assignedto_id)
+        expect(payload[:include_all]).to eq(include_all)
+        expect(payload[:case_ids]).to eq(case_ids)
         expect(payload[:unknown_parameter]).to be_nil
       end
 
