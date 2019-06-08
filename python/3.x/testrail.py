461bb9a2fe1d07e104fac56fa8b4b94a06c142b5
Allan Lewis <allanlewis99@gmail.com>
2019-06-08 14:35:28 +0100

    python: [Cleanup] Convert doc comments into docstrings

--- python/3.x/testrail.py
+++ python/3.x/testrail.py
@@ -1,15 +1,16 @@
-#
-# TestRail API binding for Python 3.x (API v2, available since
-# TestRail 3.0)
-# Compatible with TestRail 3.0 and later.
-#
-# Learn more:
-#
-# http://docs.gurock.com/testrail-api2/start
-# http://docs.gurock.com/testrail-api2/accessing
-#
-# Copyright Gurock Software GmbH. See license.md for details.
-#
+"""TestRail API binding for Python 3.x.
+
+(API v2, available since TestRail 3.0)
+
+Compatible with TestRail 3.0 and later.
+
+Learn more:
+
+http://docs.gurock.com/testrail-api2/start
+http://docs.gurock.com/testrail-api2/accessing
+
+Copyright Gurock Software GmbH. See license.md for details.
+"""
 
 import base64
 import json
@@ -26,39 +27,31 @@ class APIClient:
             base_url += '/'
         self.__url = base_url + 'index.php?/api/v2/'
 
-    #
-    # Send Get
-    #
-    # Issues a GET request (read) against the API and returns the result
-    # (as Python dict) or filepath if successful file download
-    #
-    # Arguments:
-    #
-    # uri                 The API method to call including parameters
-    #                     (e.g. get_case/1)
-    #
-    # filepath            The path and file name for attachment download
-    #                     Used only for 'get_attachment/:attachment_id'
-    #
     def send_get(self, uri, filepath=None):
+        """Issue a GET request (read) against the API.
+
+        Args:
+            uri: The API method to call including parameters, e.g. get_case/1.
+            filepath: The path and file name for attachment download; used only
+                for 'get_attachment/:attachment_id'.
+
+        Returns:
+            A dict containing the result of the request.
+        """
         return self.__send_request('GET', uri, filepath)
 
-    #
-    # Send POST
-    #
-    # Issues a POST request (write) against the API and returns the result
-    # (as Python dict).
-    #
-    # Arguments:
-    #
-    # uri                 The API method to call including parameters
-    #                     (e.g. add_case/1)
-    # data                The data to submit as part of the request (as
-    #                     Python dict, strings must be UTF-8 encoded)
-    #                     If adding an attachment, must be the path
-    #                     to the file
-    #
     def send_post(self, uri, data):
+        """Issue a POST request (write) against the API.
+
+        Args:
+            uri: The API method to call, including parameters, e.g. add_case/1.
+            data: The data to submit as part of the request as a dict; strings
+                must be UTF-8 encoded. If adding an attachment, must be the
+                path to the file.
+
+        Returns:
+            A dict containing the result of the request.
+        """
         return self.__send_request('POST', uri, data)
 
     def __send_request(self, method, uri, data):
