ebc8408545c2abacd0a556ac3fe42d95a088fceb
Dai Fujihara <daipresents@gmail.com>
2019-06-18 23:50:14 +0900

    Refactoring results_spec.rb

--- spec/lib/testrail/api/results_spec.rb
+++ spec/lib/testrail/api/results_spec.rb
@@ -4,11 +4,45 @@ RSpec.describe 'TestRail' do
   context 'API' do
     context 'Results' do
       before(:each) do
-        @client = TestRail::Client.new(TestRail.config.testrail_url)
+        @client = TestRail::Client.new
+      end
+
+      context 'result for case' do
+        before(:each) do
+          @project_id = RSpec.current_example.metadata[:project_id]
+          @case_id = RSpec.current_example.metadata[:case_id]
+          payload = @client.payload_for_adding_run
+          payload[:name] = 'Run name created by rspec'
+          @run = @client.add_run(@project_id, payload)
+        end
+
+        it 'can add/get result for case' do
+          payload = @client.payload_for_adding_result_for_case
+          payload[:status_id] = 1
+          payload[:comment] = 'comment'
+          payload[:version] = nil
+          payload[:elapsed] = '30s'
+          payload[:defects] = 'defects'
+          payload[:assignedto_id] = 1
+
+          @client.add_result_for_case(@run['id'], @case_id, payload)
+          results = @client.get_results_for_case(@run['id'], @case_id)
+
+          expect(results[0]['status_id']).to eq(payload[:status_id])
+          expect(results[0]['comment']).to eq(payload[:comment])
+          expect(results[0]['version']).to be_nil
+          expect(results[0]['elapsed']).to eq(payload[:elapsed])
+          expect(results[0]['defects']).to eq(payload[:defects])
+          expect(results[0]['assignedto_id']).to eq(payload[:assignedto_id])
+        end
+
+        after(:each) do
+          @client.close_run(@run['id'])
+        end
       end
 
       it 'get default payload' do
-        payload = @client.get_result_payload
+        payload = @client.payload_for_adding_result_for_case
 
         expect(payload[:status_id]).to be_nil
         expect(payload[:comment]).to be_nil
@@ -17,57 +51,6 @@ RSpec.describe 'TestRail' do
         expect(payload[:defects]).to be_nil
         expect(payload[:assignedto_id]).to be_nil
         expect(payload[:unknown_parameter]).to be_nil
-
-        status_id = 1
-        comment = 'comment'
-        version = 'version'
-        elapsed = '30s'
-        defects = 'defects'
-        assignedto_id = 1
-
-        payload = @client.get_result_payload(status_id, comment, version, elapsed, defects, assignedto_id)
-
-        expect(payload[:status_id]).to eq(status_id)
-        expect(payload[:comment]).to eq(comment)
-        expect(payload[:version]).to eq(version)
-        expect(payload[:elapsed]).to eq(elapsed)
-        expect(payload[:defects]).to eq(defects)
-        expect(payload[:assignedto_id]).to eq(assignedto_id)
-        expect(payload[:unknown_parameter]).to be_nil
-      end
-
-      context 'result for case' do
-        before(:each) do
-          @case_id = TestRail.config.testrail_case_id
-          @run = @client.add_run(@client.get_run_payload('Run Name', 'Run Description').compact)
-        end
-
-        it 'can add/get result for case' do
-          status_id = 1
-          comment = 'comment'
-          version = nil
-          elapsed = '30s'
-          defects = 'defects'
-          assignedto_id = 1
-
-          payload = @client.get_result_payload(
-            status_id, comment, version, elapsed, defects, assignedto_id
-          )
-
-          @client.add_result_for_case(@run['id'], @case_id, payload)
-          results = @client.get_results_for_case(@run['id'], @case_id)
-
-          expect(results[0]['status_id']).to eq(status_id)
-          expect(results[0]['comment']).to eq(comment)
-          expect(results[0]['version']).to be_nil
-          expect(results[0]['elapsed']).to eq(elapsed)
-          expect(results[0]['defects']).to eq(defects)
-          expect(results[0]['assignedto_id']).to eq(assignedto_id)
-        end
-
-        after(:each) do
-          @client.close_run(@run['id'])
-        end
       end
     end
   end
